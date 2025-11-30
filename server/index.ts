13549f90d9aacf4e8543e3eb85ea10e5e0f6fb9c
ch-sairohith <chsairohith14@gmail.com>
2025-11-30 22:01:26 +0530

    Add dotenv configuration for environment variables

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, NextFunction } from "express";
 import { createServer } from "http";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-
+import 'dotenv/config';
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
